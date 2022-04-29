e062f663486358e76428b8f1336636587febe08f
Victor Freitas <anafreitas.victor@yahoo.com>
2022-04-29 17:16:51 +0200

    responsive site example

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const submitButton = document.querySelector("form input[type=submit]")
+let inputName = document.querySelector("#nombre")
 const form = document.querySelector("form")
+
 
 form.addEventListener('submit', sendForm)
 
 function sendForm(event){
-  alert("submitted successfuly")
+  inputName = inputName.value
+  console.log(inputName)
+  alert(`Hello, ${inputName}! This is not sending emails. But if you want it to, let me know.`)
   event.preventDefault()
 }
  
